3f3f0eec3f69c5842399dd2dfdfb99227e2465f3
Crowdy <crowwwdy@gmail.com>
2023-11-09 13:31:59 +0300

    fix js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function del(){
-    var value=document.getElementById('screen').value;
-    document.getElementById('screen').value=value.substr(0,value.length-1);
-}
-
 var one=document.getElementById('one');
 one.addEventListener('click',function(){
     var body=document.querySelector('body');
@@ -21,15 +16,6 @@ two.addEventListener("click", function() {
     toggle.style.left='36%';
 })
 
-// var two=document.getElementById('two');
-// two.onclick = function(){
-//     var body=document.querySelector('body');
-//     var toggle=document.getElementById('circle');
-//     body.classList.add('active1');
-//     body.classList.remove('active2');
-//     toggle.style.left='36%';
-// }
-
 var three=document.getElementById('three');
 three.addEventListener('click',function(){
     var body=document.querySelector('body');
@@ -37,4 +23,26 @@ three.addEventListener('click',function(){
     body.classList.add('active2');
     body.classList.remove('active1');
     toggle.style.left='75%';
+})
+
+const Arrayofbuttons = document.querySelectorAll('.val')
+
+for (let i = 0; i < Arrayofbuttons.length; i++) {
+    Arrayofbuttons[i].addEventListener("click", function(){
+        document.calc.text.value+=`${Arrayofbuttons[i].innerHTML}`;
+    } )
+}
+
+
+function del(){
+    var value=document.getElementById('screen').value;
+    document.getElementById('screen').value=value.substr(0,value.length-1);
+}
+
+const dl = document.querySelector('.del');
+dl.addEventListener("click", del);
+
+const res = document.querySelector('.equal')
+dl.addEventListener("click", function(){
+    click=document.calc.text.value= eval(calc.text.value);
 })
